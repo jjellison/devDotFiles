5f41bd0ab2019a5b43809bef2e8e41a093de5aa4
Jon Jellison <jjellisonlwn@gmail.com>
2024-09-25 19:04:02 -0500

    fix syntax error in floating term

--- nvim/lua/plugins/floatingterm.lua
+++ nvim/lua/plugins/floatingterm.lua
@@ -2,7 +2,7 @@ return {
     'numToStr/FTerm.nvim',
     opts= {
         blend = 30,
-    }
+    },
     vim.keymap.set('n', '<C-b>', '<CMD>lua require("FTerm").toggle()<CR>')
 	--jkeys = function() 
 --		local builtin = require('vim-floaterm')
