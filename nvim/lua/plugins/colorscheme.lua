8ce188396013090d0a60fa1e722688ed1343a1d2
Jon Jellison <jjellisonlwn@gmail.com>
2025-07-17 21:47:12 -0500

    add proper colorscheme FINALLY

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,8 +2,6 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    lazy = false,
-
     config = function()
         vim.cmd([[colorscheme catppuccin-frappe]])
     end,
