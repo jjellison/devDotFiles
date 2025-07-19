851fb70adca74efdfa2afa9f2ae75469293dc41c
Jon Jellison <jjellisonlwn@gmail.com>
2025-07-19 02:01:27 -0500

    Add telescope bindings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,14 +1,42 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.6',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	dependencies = {
+        'nvim-lua/plenary.nvim',
+        'sharkdp/fd'
+    },
 	keys = function() 
 		local builtin = require('telescope.builtin')
-
+        local themes = require('telescope.themes')
+        --vim.key
 		return {
 			{'<leader>ff', builtin.find_files},
 			{'<leader>fg', builtin.live_grep},
 			{'<leader>fs', builtin.grep_string},
-			{'<leader>fc', builtin.current_buffer_fuzzy_find}
+			{'<leader>fc', builtin.current_buffer_fuzzy_find},
+
+            -- LSP Specific functionality
+            {'<leader>fr', builtin.lsp_references},
+            {'<leader>fd', builtin.lsp_definitions},
+            {'<leader>fi', builtin.lsp_implementations},
+            {'<leader>ft', builtin.lsp_type_definitions},
+
+            -- Git Specific Functionality
+            {'<leader>gc', builtin.git_commits},
+            {'<leader>gs', builtin.git_status},
+            {'<leader>gt', builtin.git_bcommits},
+            {'<leader>gy', builtin.git_bcommits_range},
+            {'<leader>gb', builtin.git_branches},
+
 		}
-	end
+	end,
+    opts = {
+        pickers = {
+            git_commits = {
+                theme = "dropdown",
+                -- Maybe try layout strategy??
+                --width = 1000,
+                --height = 2000,
+            }
+        },
+    },
 }
