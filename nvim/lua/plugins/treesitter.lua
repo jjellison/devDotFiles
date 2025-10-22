c9ac9f0c731d7bc40290343321f3c8cf66881440
Jon Jellison <jjellisonlwn@gmail.com>
2025-10-21 22:10:14 -0500

    add markdown suppport for treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         config = function()
             require'nvim-treesitter.configs'.setup {
                 -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-                ensure_installed = { "c", "python", "cpp", "rust", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+                ensure_installed = { "c", "python", "cpp", "rust", "toml", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
                 -- Install parsers synchronously (only applied to `ensure_installed`)
                 sync_install = false,
