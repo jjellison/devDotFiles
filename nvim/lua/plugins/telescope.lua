6db44c69774f5338e32c5b0408d8447ad5a6f6ed
jjellison <Jonathon_Jellison@bradycorp.com>
2026-01-19 14:25:40 -0600

    fix nasty popup issue where telescope was really far behind

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -35,7 +35,7 @@ end
 
 
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	'nvim-telescope/telescope.nvim', tag = 'v0.2.1',
 	dependencies = {
         'nvim-lua/plenary.nvim',
         'sharkdp/fd'
